95d6d8ce07bab977299298962114efb592dedbdb
Andrey K <andkolok@gmail.com>
2013-11-09 20:24:43 +0400

    php started

---
+++ php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <title>Registration form</title>
+       <link rel="stylesheet" href="form-basics.css" type="text/CSS" charset="utf-8"/>
+    </head>
+    <body>
+        <form action="process.php" method="post">
+        	<p>Enter your name</p><input type="text" name="name">
+        		<input type="submit" value="submit">
+        </form>
+    </body>
+</html>
+
